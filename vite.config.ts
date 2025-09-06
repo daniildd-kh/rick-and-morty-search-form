aef5f8e6067339ce0b33a186191b9db80bdfb377
Даниил <khromov865@mail.ru>
2025-09-06 18:27:09 +0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/rick-and-morty-search-form/",
   plugins: [react(), tailwindcss()],
 });
